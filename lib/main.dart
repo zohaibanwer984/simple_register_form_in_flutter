0ef8788a425c5407b0e866599b97edae5b1b92bb
Muhammed Zohaib Anwer <zohaibanwer2002@gmail.com>
2024-10-30 23:17:16 +0500

    Create Form Fields

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,92 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 50, right: 50, top: 50),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter Your Name',
+        body: Padding(
+          padding: const EdgeInsets.only(top: 100, left: 25, right: 25),
+          child: Form(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    validator: (value) {},
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your Name',
+                    ),
+                  ),
                 ),
-              ),
-            )
-          ],
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    validator: (value) {},
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your Email',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField( 
+                    validator: (value) {},
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your Password',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    validator: (value) {},
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your CNIC',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    validator: (value) {},
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your Contact',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    maxLines: 5,
+                    minLines: 3,
+                    validator: (value) {},
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your Address',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Sumbit"),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
